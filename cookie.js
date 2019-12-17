aeaccab7a742e4c37a31c9810e162f4f5388b259
blueeye113 <psk.baby0306@gmail.com>
2019-12-17 20:03:53 +0300

    feat: get websoloar data by using api

---
+++ cookie.js
@@ -0,0 +1,16 @@
+var webPage = require('webpage');
+var page = webPage.create();
+
+page.open('https://www.solarweb.com/Home/GuestLogOn?pvSystemid=4e0095f0-0673-487d-81bb-f97ad8e8b64a', function (status) {
+  var cookies = page.cookies;
+  
+  console.log('Listing cookies:');
+  for(var i in cookies) {
+    console.log(cookies[i].name + '=' + cookies[i].value);
+	// if (cookies[i].name == '__RequestVerificationToken') {
+	// 	console.log('__RequestVerificationToken = ', cookies[i].value);
+	// }
+  }
+  
+  phantom.exit();
+});
---
+++ index.js
@@ -0,0 +1,14 @@
+var path = require('path')
+var childProcess = require('child_process')
+var phantomjs = require('phantomjs')
+var binPath = phantomjs.path
+
+ 
+var childArgs = [
+  path.join(__dirname, 'cookie.js')
+]
+ 
+childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+  // handle results
+  console.log('stdout', stdout);
+})
