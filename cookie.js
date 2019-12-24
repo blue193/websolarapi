81f3b4e2772b9ed0026925bc57be6098e044827b
blueeye113 <psk.baby0306@gmail.com>
2019-12-24 21:21:55 +0300

    fix: remote comment

--- cookie.js
+++ cookie.js
@@ -4,7 +4,6 @@ var page = webPage.create();
 page.open("https://www.solarweb.com/Home/GuestLogOn?pvSystemid=4e0095f0-0673-487d-81bb-f97ad8e8b64a", function(){
 
     // this block is executed after the first page is opened
-    // var page1 = webPage.create();
     var dateObj = new Date();
     var month = dateObj.getUTCMonth() + 1; //months from 1-12
     var day = dateObj.getUTCDate();
