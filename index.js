ca9421882243b5819bc5ff2dba8899088af3bd61
blueeye113 <psk.baby0306@gmail.com>
2020-01-24 15:35:07 +0300

    fix: code indent

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var path = require('path')
 var childProcess = require('child_process')
 var phantomjs = require('phantomjs')
 var binPath = phantomjs.path
-
  
 var childArgs = [
   path.join(__dirname, 'cookie.js')
